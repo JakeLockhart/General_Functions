b81c2438a89e9f38baecf65254a4f700ae8b4a8d
Lockhart <jsl5865@psu.edu>
2025-12-02 22:59:58 -0500

    Updated FigTitle logic to account for empty arrays and '' arrays

--- SaveOpenFigures.m
+++ SaveOpenFigures.m
@@ -19,22 +19,29 @@ function SaveOpenFigures
         fprintf('\nNo destination folder selected\n')
         return
     end
+    fprintf('Saving figures...\n')
 
     OpenFigures = findall(0, "Type", "figure");
     for FigIndex = 1:numel(OpenFigures)
         Fig = OpenFigures(FigIndex);
 
-        FigData = gcf;
-        ax = findobj(FigData, "Type", "axes");
-        FigTitle = ax(1).Title.String;
-        if isempty(FigTitle)
+        FigData = findobj(Fig, "Type", "axes");
+        if isempty(FigData)
             FigTitle = sprintf('Figure%d', Fig.Number);
+        else
+            AxesTitle = string(FigData(1).Title.String);
+            switch AxesTitle
+                case ""
+                    FigTitle = sprintf('Figure%d', Fig.Number);
+                otherwise
+                    FigTitle = AxesTitle;
+            end
         end
 
         savefig(Fig, fullfile(DestinationFolder, FigTitle + ".fig"));
         exportgraphics(Fig, fullfile(DestinationFolder, FigTitle + ".jpg"));
     end
 
-    fprintf('\nAll figures saved as .fig and .jpg\n')
+    fprintf('All figures saved as .fig and .jpg\n')
 
 end
