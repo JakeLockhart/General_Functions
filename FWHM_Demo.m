d7971128c57bff8a7086d229a2f294be81a544e3
jake <jakelockhart65@gmail.com>
2025-12-21 13:45:34 -0500

    Updated function to include FWHM calculation of veins. Big inclusion is the switch case for single-peak FWHM vs double-peak FWHM. This includes synthetic demo images for arteries (representing single peak) and veins (representing double peak).

--- FWHM_Demo.m
+++ FWHM_Demo.m
@@ -1,4 +1,4 @@
-function FWHM_Demo
+function FWHM_Demo(DemoType)
     % <Documentation>
         % FWHM_Demo()
         %   
@@ -14,18 +14,45 @@ function FWHM_Demo
         %   
     % <End Documentation>
 
+arguments
+        DemoType char {mustBeMember(DemoType, {'Artery', 'Vein'})} = 'Artery';
+    end
+
     %% Synthetic Noisy Data
-        Demo = zeros(256,256, "double");                            % Create a black image that is of type 'double' in order to add noise later (0 = black, no pixel intensity)
+        Demo = zeros(256,256, "double");    % Create a black image that is of type 'double' in order to add noise later (0 = black, no pixel intensity)
+        switch DemoType
+            case 'Artery'
+                % Single Vertical Line 
+                LineThickness = randi(size(Demo, 1)/4, 1);                      % Randomly assign a line thickness 
+                YPosition.Center = size(Demo, 1) / 2;                           % Center the line along the rows 
+                YPosition.Top = ceil(YPosition.Center - LineThickness/2);       % Determine the top row (first) that the line appears
+                YPosition.Bottom = floor(YPosition.Center + LineThickness/2);   % Determine the bottom row (last) that the line appears
 
-        LineThickness = randi(size(Demo, 1)/4, 1);                  % Randomly assign a line thickness 
-        YPosition.Center = size(Demo, 1) / 2;                       % Center the line along the rows 
-        YPosition.Top = ceil(YPosition.Center - LineThickness/2);     % Determine the top row (first) that the line appears
-        YPosition.Bottom = floor(YPosition.Center + LineThickness/2); % Determine the bottom row (last) that the line appears
-        Demo(YPosition.Top:YPosition.Bottom, :) = 1;                % Give all rows between the top/bottom rows of the line an intensity (1 = fully bright, max pixel intensity)
+                Demo(YPosition.Top:YPosition.Bottom, :) = 1;                    % Give all rows between the top/bottom rows of the line an intensity (1 = fully bright, max pixel intensity)
 
-        Demo = Demo + 0.1*randn(size(Demo));                        % Add random noise to the entire image (salt and pepper noise)
-        Demo = min(max(Demo, 0), 1);                                % Lock all pixel intensities between 0 and 1
-        Demo = imnoise(Demo, "gaussian", 0, 0.005);                 % Add gaussian noise to the entire image
+                VesselWidth = LineThickness;
+
+            case 'Vein'
+                % Vertical Line with Pronounced Edges
+                LineThickness = randi(size(Demo, 1)/4, [1,2]);
+                YPosition.Center = [randi(size(Demo, 1)/4, 1), size(Demo, 1)/2 + randi(size(Demo, 1)/4, 1)];
+                YPosition.Top = [ceil(YPosition.Center(1) - LineThickness(1)/2), ceil(YPosition.Center(2) - LineThickness(1)/2)];
+                YPosition.Bottom = [ceil(YPosition.Center(1) + LineThickness(1)/2), ceil(YPosition.Center(2) + LineThickness(1)/2)];
+
+                MinGapIntensity = 0.25;
+                MaxGapIntensity = 1;
+                GapIntensity = (MaxGapIntensity - MinGapIntensity)*rand();
+
+                Demo(YPosition.Top(1):YPosition.Bottom(1), :) = MaxGapIntensity;
+                Demo(YPosition.Top(2):YPosition.Bottom(2), :) = MaxGapIntensity;
+                Demo(YPosition.Bottom(1):YPosition.Top(2), :) = GapIntensity;
+                
+                VesselWidth = YPosition.Bottom(2) - YPosition.Top(1);
+        end
+
+        Demo = Demo + 0.1*randn(size(Demo));            % Add random noise to the entire image (salt and pepper noise)
+        Demo = min(max(Demo, 0), 1);                    % Lock all pixel intensities between 0 and 1
+        Demo = imnoise(Demo, "gaussian", 0, 0.005);     % Add gaussian noise to the entire image
 
     %% Create Figure
         figure;             % Create a figure window to contain all plots
@@ -33,7 +60,7 @@ function FWHM_Demo
 
         Tile1 = nexttile;                                                                   % Define first tile
         imshow(Demo, [], 'Parent', Tile1);                                                  % Display the synthetic data
-        title(Tile1, sprintf('Demo image - Draw ROI\n(%g Pixel Width)', LineThickness));    % Give a title to tile that contains true line thickness
+        title(Tile1, sprintf('Demo image - Draw ROI\n(%g Pixel Width)', VesselWidth));      % Give a title to tile that contains true line thickness
 
         Tile2 = nexttile;                                                                           % Define second tile
         hold on;                                                                                    % Hold all plotted components on tile
@@ -74,15 +101,27 @@ function FWHM_Demo
 
             ROILength = linspace(0,1,length(ROIProfile)) * norm(diff(ROI.Position));
 
-            [FWHM, Edge1, Edge2] = CalculateFWHM(ROILength, ROIProfile);
+            switch DemoType
+                case 'Artery'
+                    [FWHM, Edge1, Edge2] = CalculateFWHM(ROILength, ROIProfile);
 
+                    set(ROIProfilePlot, 'XData', ROILength, 'YData', ROIProfile);
+                    set(IntensityMidLine, 'Label', sprintf('FWHM ≈ %.1f px', FWHM));
+                    set(IntensityMidLine, 'Value', 0.5 * (max(ROIProfile) + min(ROIProfile)));
+                    set(Edge1Line, 'Value', Edge1);
+                    set(Edge2Line, 'Value', Edge2);
+
+                case 'Vein'
+                    [FWHM, Edge1, Edge2, ~, ~] = CalculateFWHMEdge(ROILength, ROIProfile);
+            end
+            
             set(ROIProfilePlot, 'XData', ROILength, 'YData', ROIProfile);
-            set(IntensityMidLine, 'Label', sprintf('FWHM ≈ %.1f px', FWHM));
-            set(IntensityMidLine, 'Value', 0.5 * (max(ROIProfile) + min(ROIProfile)));
+            set(IntensityMidLine, 'Label', sprintf('Total FWHM ≈ %.1f px', FWHM));
+            set(IntensityMidLine, 'Value', 0.5*(max(ROIProfile)+min(ROIProfile)));
             set(Edge1Line, 'Value', Edge1);
             set(Edge2Line, 'Value', Edge2);
 
-            title(Tile1, sprintf('Demo image - Draw ROI\n(%g Pixel Width)', LineThickness));
+            title(Tile1, sprintf('Demo image - Draw ROI\n(%g Pixel Width)', VesselWidth));
             drawnow
         end
 end
@@ -109,22 +148,39 @@ function ROIProfile = AdjustedImprofile(Demo, x, y, ROI_HalfWidth, SamplesOnROI)
     ROIProfile = mean(Temp, 2, 'omitnan');
 end
 
-function [FWHM, Edge1, Edge2] = CalculateFWHM(x, y)
-    y = y - min(y);
-    y = y / max(y);
+function [FWHM, Edge1, Edge2] = CalculateFWHM(ROIProfilePosition, Intensity)
 
-    HalfMax = 0.5;
+    MinIntensity = min(Intensity);
+    MaxIntensity = max(Intensity);
 
-    UpperRegion = y >= HalfMax;
-    idx = find(UpperRegion);
-    if isempty(idx)
+    scale = 1 / (MaxIntensity - MinIntensity);
+    Intensity = (Intensity - MinIntensity) * scale;
+
+    Index = find(Intensity >= 0.5);
+    if isempty(Index)
         FWHM = NaN;
         Edge1 = NaN;
         Edge2 = NaN;
         return
     end
 
-    Edge1 = x(idx(1));
-    Edge2 = x(idx(end));
+    Edge1 = ROIProfilePosition(Index(1));
+    Edge2 = ROIProfilePosition(Index(end));
     FWHM = Edge2 - Edge1;
+end
+
+function [TotalFWHM, Edge1, Edge2, EdgeFWHM_Left, EdgeFWHM_Right] = CalculateFWHMEdge(ROIProfilePosition, Intensity)
+    N = numel(ROIProfilePosition);
+    MidIdx = floor(N/2);
+
+    [FWHM_Left, Edge1_Left, Edge2_Left] = CalculateFWHM(ROIProfilePosition(1:MidIdx), Intensity(1:MidIdx));
+    [FWHM_Right, Edge1_Right, Edge2_Right] = CalculateFWHM(ROIProfilePosition(MidIdx+1:end), Intensity(MidIdx+1:end));
+
+    EdgeFWHM_Left = FWHM_Left;
+    EdgeFWHM_Right = FWHM_Right;
+
+    Edge1 = Edge1_Left;   % left-most edge
+    Edge2 = Edge2_Right;  % right-most edge
+
+    TotalFWHM = Edge2 - Edge1;
 end
