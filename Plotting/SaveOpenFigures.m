cddfae32ed5e11608ae6f706d94b44c8ddd1f90c
Jake Lockhart <jsl5865@psu.edu>
2026-09-07 18:44:47 -0400

    Syntax update and Bug fix: Files of the same title were overwritting each other. Fixed with indexing titles.

--- Plotting/SaveOpenFigures.m
+++ Plotting/SaveOpenFigures.m
@@ -1,4 +1,4 @@
-function SaveOpenFigures(FileType, DestinationFolder)
+function SaveopenFigures(fileType, destinationFolder)
     % <Documentation>
         % SaveAllFigures()
         %   
@@ -14,74 +14,89 @@ function SaveOpenFigures(FileType, DestinationFolder)
         %   
     % <End Documentation>
     arguments
-        FileType string {mustBeVector, mustBeMember(FileType, ["fig", "jpg", "tif", "gif", "png", "eps", "svg"])}
-        DestinationFolder (1,1) string {mustBeFolderOrEmpty} = ""
+        fileType string {mustBeVector, mustBeMember(fileType, ["fig", "jpg", "tif", "gif", "png", "eps", "svg"])}
+        destinationFolder (1,1) string {mustBeFolderOrEmpty} = ""
     end
 
-    if DestinationFolder == ""
-        DestinationFolder = uigetdir(pwd, "Choose a folder to save all open figures...");
-        if DestinationFolder == 0
+    if destinationFolder == ""
+        destinationFolder = uigetdir(pwd, "Choose a folder to save all open figures...");
+        if destinationFolder == 0
             fprintf('\nNo destination folder selected\n')
             return
         end
     end
     fprintf('Saving figures...\n')
 
-    ChildFolders = arrayfun(@(ext) CreateDirectory(ext, DestinationFolder), FileType, "UniformOutput", false);
+    childFolders = arrayfun(@(ext) CreateDirectory(ext, destinationFolder), fileType, "UniformOutput", false);
 
-    OpenFigures = findall(0, "Type", "figure");
-    for FigIndex = 1:numel(OpenFigures)
-        Fig = OpenFigures(FigIndex);
-        FigureTitle = GetFigureTitle(Fig);
-        for Child = 1:numel(FileType)
-            ext = FileType(Child);
-            ChildFolder = ChildFolders{Child};
+    openFigures = findall(0, "Type", "figure");
+    existingTitles = strings(0, numel(openFigures));
+    for figIndex = 1:numel(openFigures)
+        Fig = openFigures(figIndex);
+
+        figureTitle = GetfigureTitle(Fig);
+        figureTitle = ValidateUniqueTitle(figureTitle, existingTitles);
+        existingTitles(figIndex) = figureTitle;
+
+        for child = 1:numel(fileType)
+            ext = fileType(child);
+            childFolder = childFolders{child};
             switch ext
                 case "fig"
-                    savefig(Fig, fullfile(ChildFolder, FigureTitle + "." + ext));
+                    savefig(Fig, fullfile(childFolder, figureTitle + "." + ext));
                 otherwise
-                    exportgraphics(Fig, fullfile(ChildFolder, FigureTitle + "." + ext));
+                    exportgraphics(Fig, fullfile(childFolder, figureTitle + "." + ext));
             end
         end
     end
 
 
-    fprintf('All figures saved as %s\n', strjoin(strcat('.', FileType), ', '))
+    fprintf('All figures saved as %s\n', strjoin(strcat('.', fileType), ', '))
 
     function extFolder = CreateDirectory(ext, ParentFolder)
-        Title = sprintf('MatLab Figures (.%s)', ext);
-        extFolder = fullfile(ParentFolder, Title);
+        title = sprintf('MatLab Figures (.%s)', ext);
+        extFolder = fullfile(ParentFolder, title);
         if ~exist(extFolder, 'dir')
             mkdir(extFolder)
         end
     end
 
-    function FigureTitle = GetFigureTitle(Fig)
-        Tiles = findobj(Fig, "Type", "tiledlayout");
+    function figureTitle = GetfigureTitle(fig)
+        Tiles = findobj(fig, "Type", "tiledlayout");
         if ~isempty(Tiles)
-            TileAx = Tiles(1);
-            if ~isempty(TileAx.Title) && ~isempty(TileAx.Title.String)
-                FigureTitle = CleanFileName(string(TileAx.Title.String));
+            tileAx = Tiles(1);
+            if ~isempty(tileAx.Title) && ~isempty(tileAx.Title.String)
+                figureTitle = CleanFileName(string(tileAx.Title.String));
                 return
             end
         end
 
-        Axes = findobj(Fig, "Type", "axes");
-        for ax = Axes'
+        axes = findobj(fig, "Type", "axes");
+        for ax = axes'
             if ~isempty(ax.Title) && ~isempty(ax.Title.String)
-                FigureTitle = CleanFileName(string(ax.Title.String));
+                figureTitle = CleanFileName(string(ax.Title.String));
                 return
             end
         end
 
-        FigureTitle = sprintf('Figure %d', Fig.Number);
+        figureTitle = sprintf('Figure %d', fig.Number);
     end
 
-    function AppropriateFileName = CleanFileName(FigureName)
-        if isstring(FigureName) || ischar(FigureName)
-            FigureName = strjoin(string(FigureName), '_');
+    function uniqueTitle = ValidateUniqueTitle(baseTitle, existingTitles)
+        uniqueTitle = baseTitle;
+        count = 1;
+        
+        while any(existingTitles == uniqueTitle)
+            uniqueTitle = sprintf('%s (%d)', baseTitle, count);
+            count = count + 1;
         end
-        AppropriateFileName = regexprep(FigureName, '[<>:"/\\|?*]', '_');
+    end
+
+    function appropriateFileName = CleanFileName(figureName)
+        if isstring(figureName) || ischar(figureName)
+            figureName = strjoin(string(figureName), '_');
+        end
+        appropriateFileName = regexprep(figureName, '[<>:"/\\|?*]', '_');
     end
 
 end
